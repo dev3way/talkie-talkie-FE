db82666c02befd3bde7e80b44e476e089d222f2d
rolled-potatoes <keep4404@gmail.com>
2021-03-25 15:11:41 +0900

    fix : eslint 파일 문법 검사
    prettier 적용이 안되어 적용함.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,5 @@
 module.exports = {
   root: true,
   extends: ['airbnb', 'plugin:jsx-a11y/recommended', '@react-native-community'],
-  rules: {'react/jsx-filename-extension': [1, {extensions: ['.tsx']}]},
+  rules: { 'react/jsx-filename-extension': [1, { extensions: ['.tsx'] }] },
 };
