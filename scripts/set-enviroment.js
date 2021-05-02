f5bdf82fbe76dcc354ddf6e6826b5e1f6798c5db
rolled-potatoes <keep4404@gmail.com>
2021-05-02 20:32:07 +0900

    chore : 환경변수 설정 및 eslint 설정변경
    환경변수를 json으로 불러올 수 있도록 변경.

---
+++ scripts/set-enviroment.js
@@ -0,0 +1,8 @@
+#! /bin/node
+/* eslint-disable import/no-dynamic-require */
+
+const fs = require('fs');
+
+const environment = process.argv[2];
+const envFileContent = require(`../envs/${environment}.json`);
+fs.writeFileSync('env.json', JSON.stringify(envFileContent, undefined, 2));
